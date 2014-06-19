916501ded259d30e19169656be709831feaeba5c
Kouhei Maeda <mkouhei@palmtb.net>
2014-06-19 18:17:50 +0900

    Added PyPI user_packages() of XML-RPC.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- main.go
+++ main.go
@@ -13,12 +13,9 @@ var (
 	email    = "mkouhei@palmtb.net"
 	udd      = "http://udd.debian.org/dmd/?format=json"
 	pypi     = "http://pypi.python.org/pypi"
+	mozilla  = "https://bugzilla.mozilla.org/xmlrpc.cgi"
 	github   = "https://api.github.com/users"
 )
-
-type userPackages struct {
-	UserPackages string `xmlrpc:"user_packages"`
-}
 
 func client(s string) {
 	resp, err := http.Get(s)
@@ -33,11 +30,66 @@ func client(s string) {
 	log.Println()
 }
 
+type pkg struct {
+	Role string
+	Name string
+}
+
+type pkgs struct {
+	Pkgs []pkg `xmlrpc:"array"`
+}
+
 func xmlRpcClient(s string) {
 	client, _ := xmlrpc.NewClient(s, nil)
-	result := &userPackages{}
+	defer client.Close()
+	/*
+			// Bugzill.version
+			<?xml version="1.0" encoding="UTF-8"?>
+			<methodResponse>
+			<params>
+			<param>
+			<value>
+			<struct>
+			<member>
+			<name>version</name>
+			<value>
+			<string>4.2.9+</string>
+			</value></member>
+			</struct>
+			</value>
+			</param>
+			</params>
+			</methodResponse>
+		---
+					var result = struct {
+						Version string `xmlrpc:"version"`
+					}{}
+					client.Call("Bugzilla.version", nil, &result)
+	*/
+
+	/*
+		//PyPI package_releases()
+		---
+		<?xml version='1.0'?>
+		<methodResponse>
+		<params>
+		<param>
+		<value><array><data>
+		<value><string>0.3.4</string></value>
+		</data></array></value>
+		</param>
+		</params>
+		</methodResponse>
+		---
+			var result []string
+			client.Call("package_releases", "shiori", &result)
+	*/
+
+	// PyPI user_packages()
+	var result [][]string
 	client.Call("user_packages", "mkouhei", &result)
-	fmt.Printf("%s\n", result.UserPackages)
+
+	fmt.Println(result)
 }
 
 func assert(data interface{}) {
@@ -70,7 +122,8 @@ func assert(data interface{}) {
 }
 
 func main() {
-	//client(udd + "&email1=" + email)
-	//client(github + "/" + username + "/events")
+	client(udd + "&email1=" + email)
+	client(github + "/" + username + "/events")
+	//xmlRpcClient(mozilla)
 	xmlRpcClient(pypi)
 }
