f42a546cf58922fad59333eb8d445b40c1f1c23d
Kouhei Maeda <mkouhei@palmtb.net>
2014-08-11 21:23:54 +0900

    Added unit test of restClient().
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ restcli_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"testing"
+
+	"github.com/bitly/go-simplejson"
+)
+
+func TestRestClient(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		js := simplejson.New()
+		js.Set("hello", "world")
+		data, _ := js.EncodePretty()
+		fmt.Fprintln(w, string(data))
+	}))
+	defer ts.Close()
+	js := restClient(ts.URL)
+	if js == nil {
+		t.Errorf("%v", js)
+	}
+	if v, err := js.Get("hello").String(); err != nil {
+		t.Errorf("%v, want: world\n", v)
+	}
+}
