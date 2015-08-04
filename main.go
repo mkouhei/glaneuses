e8c5e22633cb8b94fbe406a71c376bb5e14cb7a6
Kouhei Maeda <mkouhei@palmtb.net>
2015-08-04 12:41:38 +0900

    Updates variable definition.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- main.go
+++ main.go
@@ -17,17 +17,20 @@ type config struct {
 	services []service
 }
 
-var srvMap map[string]string = map[string]string{
-	"debian":    `https://udd.debian.org/dmd/?email1=%s`,
-	"pypi":      "https://pypi.python.org/pypi",
-	"rubygems":  `https://rubygems.org/api/v1/owners/%s/gems.json`,
-	"github":    `https://api.github.com/users/%s/events`,
-	"bitbucket": `https://bitbucket.org/api/1.0/users/%s/events`,
-	"pgp":       `http://pgp.mit.edu/pks/lookup?op=index&fingerprint=on&search=%s`,
+var (
+	srvMap map[string]string = map[string]string{
+		"debian":    `https://udd.debian.org/dmd/?email1=%s`,
+		"pypi":      "https://pypi.python.org/pypi",
+		"rubygems":  `https://rubygems.org/api/v1/owners/%s/gems.json`,
+		"github":    `https://api.github.com/users/%s/events`,
+		"bitbucket": `https://bitbucket.org/api/1.0/users/%s/events`,
+		"pgp":       `http://pgp.mit.edu/pks/lookup?op=index&fingerprint=on&search=%s`,
+	}
+	version     string
+	showVersion = flag.Bool("version", false, "showVersion")
+)
 }
 
-var version string
-var showVersion = flag.Bool("version", false, "showVersion")
 
 func main() {
 	c := flag.String("c", "glaneuses.conf", "Configuration file")
