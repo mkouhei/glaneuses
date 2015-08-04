4d35fe051be51815a6a94b0cb0a2c623500da203
Kouhei Maeda <mkouhei@palmtb.net>
2015-08-05 06:44:32 +0900

    Moves function and variable related net/http to server.go.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"net/http"
 	"time"
 )
 
@@ -30,35 +28,7 @@ var (
 	}
 	version     string
 	showVersion = flag.Bool("version", false, "showVersion")
-	server      = &http.Server{Addr: ":8080"}
 )
-
-func (conf *config) app() {
-	http.Handle("/", http.HandlerFunc(conf.serveJSON))
-	if err := http.ListenAndServe(server.Addr, nil); err != nil {
-		log.Fatalf("ListenAndServe: %s", err)
-	}
-}
-
-func (conf *config) serveJSON(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	data, err := conf.mergeJSON()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprintf(w, string(data))
-}
-
-func (conf *config) checkEtag(url string) string {
-	resp, err := http.Head(url)
-	if err != nil {
-		return ""
-	}
-	if len(resp.Header["Etag"]) > 0 {
-		return resp.Header["Etag"][0]
-	}
-	return fmt.Sprintf("expire-%d", int32(time.Now().Add(conf.expire).Unix()))
-}
 
 func main() {
 	c := flag.String("c", "glaneuses.conf", "Configuration file")
--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/bitly/go-simplejson"
 )
+
+var server = &http.Server{Addr: ":8080"}
 
 func (conf *config) mergeJSON() ([]byte, error) {
 	js := simplejson.New()
@@ -50,4 +54,31 @@ func (conf *config) mergeJSON() ([]byte, error) {
 		return nil, err
 	}
 	return data, nil
+}
+
+func (conf *config) app() {
+	http.Handle("/", http.HandlerFunc(conf.serveJSON))
+	if err := http.ListenAndServe(server.Addr, nil); err != nil {
+		log.Fatalf("ListenAndServe: %s", err)
+	}
+}
+
+func (conf *config) serveJSON(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := conf.mergeJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, string(data))
+}
+
+func (conf *config) checkEtag(url string) string {
+	resp, err := http.Head(url)
+	if err != nil {
+		return ""
+	}
+	if len(resp.Header["Etag"]) > 0 {
+		return resp.Header["Etag"][0]
+	}
+	return fmt.Sprintf("expire-%d", int32(time.Now().Add(conf.expire).Unix()))
 }
